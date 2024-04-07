0b9c2ac3a5d909d0cf2826124b1e6cd3117e959c
Guillaume Docquier <guillaumedocquier@hotmail.com>
2024-04-07 17:52:31 -0400

    Added a trigger for the RecordNewGame workflow

---
+++ triggers/RecordGameResultTrigger.ts
@@ -0,0 +1,24 @@
+import { Trigger } from "deno-slack-sdk/types.ts";
+import { TriggerContextData, TriggerTypes } from "deno-slack-api/mod.ts";
+import RecordGameResultWorkflow from '../workflows/RecordGameResultWorkflow.ts'
+
+/**
+ * A trigger to start the RecordGameResult Workflow
+ * https://api.slack.com/automation/triggers
+ */
+const RecordGameResultTrigger: Trigger<typeof RecordGameResultWorkflow.definition> = {
+  type: TriggerTypes.Shortcut,
+  name: "RecordGameResult trigger",
+  description: "Starts the workflow to record a new game.",
+  workflow: `#/workflows/${RecordGameResultWorkflow.definition.callback_id}`,
+  inputs: {
+    interactivity: {
+      value: TriggerContextData.Shortcut.interactivity,
+    },
+    user: {
+      value: TriggerContextData.Shortcut.user_id,
+    },
+  },
+};
+
+export default RecordGameResultTrigger;
